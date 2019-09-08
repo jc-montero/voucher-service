12f40c698a438ec9d6f461a99ef861ece56eb2f3
jcmontero <jc.montero.bonilla@gmail.com>
2019-09-08 22:56:12 +0200

    Testing the repositories

---
+++ src/test/java/com/mrjeff/voucherservice/repository/VoucherRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.mrjeff.voucherservice.repository;
+
+import com.mrjeff.voucherservice.model.Voucher;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VoucherRepositoryTest {
+
+    @Autowired
+    private VoucherRepository repository;
+
+    @Test
+    public void findAll() {
+        List<Voucher> vouchers = repository.findAll();
+        Assert.assertEquals(vouchers.size(), 7);
+    }
+
+    @Test
+    public void findByCode() {
+        Voucher voucher = repository.findByCode("CUPON-A");
+        Assert.assertNotNull(voucher);
+    }
+}
