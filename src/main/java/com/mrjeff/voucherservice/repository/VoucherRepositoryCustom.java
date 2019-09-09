bbd9d29cfb63fc562c9f3eb958d2dadebab963ba
jcmontero <jc.montero.bonilla@gmail.com>
2019-09-09 21:35:24 +0200

    New method to find vouchers by active and discount

--- src/main/java/com/mrjeff/voucherservice/repository/VoucherRepositoryCustom.java
+++ src/main/java/com/mrjeff/voucherservice/repository/VoucherRepositoryCustom.java
@@ -2,6 +2,9 @@ package com.mrjeff.voucherservice.repository;
 
 import com.mrjeff.voucherservice.model.Voucher;
 
+import java.util.List;
+
 public interface VoucherRepositoryCustom {
     Voucher findByCode(String code);
+    List<Voucher> findAllByActiveAndDiscount(boolean active, String discountCode);
 }
--- src/main/java/com/mrjeff/voucherservice/repository/impl/VoucherRepositoryImpl.java
+++ src/main/java/com/mrjeff/voucherservice/repository/impl/VoucherRepositoryImpl.java
@@ -3,11 +3,11 @@ package com.mrjeff.voucherservice.repository.impl;
 import com.mrjeff.voucherservice.model.Voucher;
 import com.mrjeff.voucherservice.repository.VoucherRepositoryCustom;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 
 @Slf4j
 public class VoucherRepositoryImpl implements VoucherRepositoryCustom {
@@ -27,6 +27,24 @@ public class VoucherRepositoryImpl implements VoucherRepositoryCustom {
             result = (Voucher) query.getSingleResult();
         } catch (Exception e ) {
             log.error("Error trying to retrieve the voucher with the code: {}", code, e);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<Voucher> findAllByActiveAndDiscount(boolean active, String discountCode) {
+        List<Voucher> result = null;
+
+        try {
+            Query query = entityManager.createNativeQuery("SELECT v.* FROM voucher as v, discount as d" +
+                    " WHERE v.discount=d.id AND v.active = :active AND d.code = :discountCode", Voucher.class);
+            query.setParameter("active", active);
+            query.setParameter("discountCode", discountCode);
+
+            result = query.getResultList();
+        } catch (Exception e) {
+            log.error("Error trying to retrieve all vouchers with: active={}, discount={}", active, discountCode, e);
         }
 
         return result;
