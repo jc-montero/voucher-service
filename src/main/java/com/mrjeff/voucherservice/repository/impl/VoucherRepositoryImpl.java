27c7188c77a73cac5eb10fad2d2028e0953f9ebb
jcmontero <jc.montero.bonilla@gmail.com>
2019-09-09 20:14:09 +0200

    Annotation not needed

--- src/main/java/com/mrjeff/voucherservice/repository/impl/VoucherRepositoryImpl.java
+++ src/main/java/com/mrjeff/voucherservice/repository/impl/VoucherRepositoryImpl.java
@@ -9,7 +9,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-@Repository
 @Slf4j
 public class VoucherRepositoryImpl implements VoucherRepositoryCustom {
 
